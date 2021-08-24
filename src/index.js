6f93dce16845a48a64d8e1edd9e040674c39c176
Anidamp <79271758+Anidamp@users.noreply.github.com>
2021-08-24 15:44:31 +0300

    final

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import menuTemplate from "./templates/template.hbs";
 
 const list = document.querySelector('.js-menu');
 
-//   const markup = items.map(menuTemplate).join('');
+
  const markup = menuTemplate(items);
  list.insertAdjacentHTML('afterbegin', markup);
